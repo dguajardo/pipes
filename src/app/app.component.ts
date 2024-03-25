77031d6fe8662896d576e78e8a0d4ae28dddcc80
DGT <diego.guajardo@hotmail.cl>
2024-03-25 13:05:22 -0300

    feat: some piping

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {  UpperCasePipe, LowerCasePipe, SlicePipe }  from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, // <--- add this comment
+    UpperCasePipe,
+    LowerCasePipe,
+    SlicePipe,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = 'Captain America';
+  date: Date = new Date();
+  array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 }
